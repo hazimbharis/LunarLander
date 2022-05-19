514b076588b72356b1a6c299419d574dbf51fec5
Zim <98758788+hazimbharis@users.noreply.github.com>
2022-05-19 20:59:16 +0100

    tried to make realistic momentum and canvas overlay

--- lunarlander.c
+++ lunarlander.c
@@ -17,7 +17,7 @@
  *
  */
 
-#define FPS 2
+#define FPS 1
 
 typedef struct
 {
@@ -45,8 +45,10 @@ typedef struct
 
 Entity* player; // change this somehow
 int fuel;      // move to player struct?
-int hvelocity; // move to player struct?
+int hvelocity = 10; // move to player struct?
 int vvelocity; // move to player struct?
+int score;
+int counter;
 
 // change this player initialization bit too, hmm
 Entity* createPlayer(Position start_pos)
@@ -66,8 +68,8 @@ void handleInput(int input)
   {
     //move up
     case KEY_UP:
+    mvaddch(player->pos.y, player->pos.x, ' ');
       player->pos.y--;
-      // mvaddch " " over current spot
       // fuel decrease
       // does not visually show ship going up unless vvelocity is negative
       // delay reset
@@ -75,26 +77,29 @@ void handleInput(int input)
       break;
     //move down
     case KEY_DOWN:
+    mvaddch(player->pos.y, player->pos.x, ' ');
       player->pos.y++;
-      // mvaddch " " over current spot
       // fuel decrease
       // downward velocity increase
       break;
     //move left
     case KEY_LEFT:
-      player->pos.x--;
-      // mvaddch " " over current spot
+    mvaddch(player->pos.y, player->pos.x, ' ');
+      //player->pos.x--;
+      hvelocity--;
       // fuel decrease
       // negative velocity increase
       break;
     //move right
     case KEY_RIGHT:
-      player->pos.x++;
-      // mvaddch " " over current spot
+    mvaddch(player->pos.y, player->pos.x, ' ');
+      //player->pos.x++;
+      hvelocity++;
       // fuel decrease
       // positive velocity increase
       break;
     default:
+    mvaddch(player->pos.y, player->pos.x, ' ');
       break;
   }
 
@@ -109,8 +114,16 @@ void generateTerrain() {
   ;
 }
 
-void overlayCanvas() {
+void overlayCanvas(int height, int width) {
   // prints score?, time, fuel, hvelocity, vvelocity and altitude at the top of the screen
+  mvprintw(0,0,"SCORE %d", score);
+  mvprintw(2,0,"COUNTER %d", counter);
+  mvprintw(4,0,"FUEL %d", fuel);
+
+  mvprintw(0,width-10,"ALTITUDE");
+  //mvprintw(2,width-10,"X-VELOCITY %d", hvelocity);
+  mvprintw(4,width-10,"Y-VELOCITY");
+
 
 }
 
@@ -118,6 +131,7 @@ int main()
 {	
   int ch;
   int delay = 0;
+  int height,width;
   Position start_pos = {10, 0};
   // add starting hvelocity, vvelocity, and altitude - maybe put in player struct
 
@@ -127,6 +141,8 @@ int main()
   keypad(stdscr, TRUE);
   nodelay(stdscr, TRUE);
 
+  getmaxyx(stdscr,height,width);
+  overlayCanvas(height,width);
   player = createPlayer(start_pos);
   mvaddch(player->pos.y, player->pos.x, player->ch);
 
@@ -136,17 +152,26 @@ int main()
       break;
     }
 
-    //if (hvelocity > 0)
-    player->pos.x += 1;
+    if (hvelocity > 0) {
+      mvaddch(player->pos.y, player->pos.x, ' ');
+      player->pos.x += 1;
+      mvaddch(player->pos.y, player->pos.x, player->ch);
+    }
+
+    if (hvelocity < 0) {
+      mvaddch(player->pos.y, player->pos.x, ' ');
+      player->pos.x -= 1;
+      mvaddch(player->pos.y, player->pos.x, player->ch);
+    }
     
-    //if (hvelocity < 0)
-    // go left
 
     delay++;
     
     //if (vvelocity > 0)
     if (delay == 2) {
+      mvaddch(player->pos.y, player->pos.x, ' ');
       player->pos.y += 1;
+      mvaddch(player->pos.y, player->pos.x, player->ch);
       delay = 0;
     }
 
@@ -154,8 +179,9 @@ int main()
     //
 
     handleInput(ch);
-    clear(); // need to change to either refresh() or smtg else because it will delete terrain
+    //clear(); // need to change to either refresh() or smtg else because it will delete terrain and canvas
     mvaddch(player->pos.y, player->pos.x, player->ch);
+    mvprintw(2,width-16,"X-VELOCITY %d", hvelocity);
 
     while(getch() != ERR) {} // emptys getch buffer so no input lag
     napms(1000/FPS);
